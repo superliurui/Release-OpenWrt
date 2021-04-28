6677cec26e796f70bc97084362ed3b682b708451
jiawm <42288069+jiawm@users.noreply.github.com>
2021-04-28 20:45:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,7 +28,7 @@ git clone https://github.com/siropboy/sirpdboy-package package/sirpdboy-package
 #git clone https://github.com/kenzok8/openwrt-packages.git package/kenzo
 #git clone https://github.com/jiawm/luci-app-poweroff.git package/poweroff
 
-rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #删除自带argon主题，安装新的argon主题
+#rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #删除自带argon主题，安装新的argon主题
 #全新的[argon-主题]此主题玩法很多,这里看说明【https://github.com/jerrykuku/luci-theme-argon/blob/18.06/README_ZH.md】
 #增加可自定义登录背景功能，请自行将文件上传到/www/luci-static/argon/background 目录下，支持jpg png gif格式图片，主题将会优先显示自定义背景，多个背景为随机显示，系统默认依然为从bing获取
 #增加了可以强制锁定暗色模式的功能，如果需要，请登录ssh 输入：touch /etc/dark 即可开启，关闭请输入：rm -rf /etc/dark，关闭后颜色模式为跟随系统

f4a6bfd40d6ef9b00b42b199a3bccb4c73950cb6
jiawm <42288069+jiawm@users.noreply.github.com>
2021-05-05 06:09:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,11 +10,11 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default  #使用源码自带ShadowSocksR Plus+出国软件
 
 # Add a feed source 添加更新源
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#sed -i '$a src-git packages https://github.com/coolsnowwolf/packages
-#sed -i '$a src-git luci https://github.com/coolsnowwolf/luci
-#sed -i '$a src-git routing https://git.openwrt.org/feed/routing.git
-#sed -i '$a src-git telephony https://git.openwrt.org/feed/telephony.git
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git packages https://github.com/coolsnowwolf/packages
+sed -i '$a src-git luci https://github.com/coolsnowwolf/luci
+sed -i '$a src-git routing https://git.openwrt.org/feed/routing.git
+sed -i '$a src-git telephony https://git.openwrt.org/feed/telephony.git
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages
 

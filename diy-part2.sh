8927e97ec04f7a7cb4a1ce430c4035c551b9603b
jiawm <42288069+jiawm@users.noreply.github.com>
2021-05-05 07:14:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,7 @@ sed -i 's/system/control/g' package/sirpdboy-package/luci-app-cpulimit/luasrc/co
 #修改定时设置为管控模式下
 sed -i 's/system/control/g' package/sirpdboy-package/luci-app-autotimeset/luasrc/controller/autotimeset.lua
 #修改IP限速（eqos）为管控模式下
-sed -i 's/network/control/g' package/sirpdboy-package/luci-app-autotimeset/luasrc/controller/autotimeset.lua
+sed -i 's/network/control/g' package/sirpdboy-package/luci-app-eqos/luasrc/controller/eqos.lua 
 #修改网络唤醒为管控模式下
 sed -i 's/services/control/g' feeds/luci/applications/luci-app-wol/luasrc/controller/wol.lua 
 #修改磁盘休眠为管控模式下

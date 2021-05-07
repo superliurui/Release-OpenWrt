8d1bab8d8e19bebfe21f14eae55718ba58193f82
jiawm <42288069+jiawm@users.noreply.github.com>
2021-05-07 12:50:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,14 +20,13 @@ sed -i '$a src-git telephony https://git.openwrt.org/feed/telephony.git
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages
 
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/lienol
-git clone https://github.com/fw876/helloworld.git package/hellowrold
+#git clone https://github.com/fw876/helloworld.git package/hellowrold #上面命令已经取消helloword #
 git clone https://github.com/siropboy/sirpdboy-package package/sirpdboy-package
 git clone https://github.com/linkease/nas-packages.git package/nas #添加ddnsto和linkease
 git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset #添加sirpdboy定时设置
 git clone https://github.com/jiawm/luci-app-poweroff.git package/poweroff
 git clone https://github.com/tty228/luci-app-serverchan.git package/serverchan
 #git clone https://github.com/kenzok8/openwrt-packages.git package/kenzo
-#git clone https://github.com/sirpdboy/luci-app-netdata cp -r luci-app-netdata LEDE_DIR/package/luci-app-netdata
 
 
 rm -rf ./package/lean/luci-app-netdata && git clone https://github.com/sirpdboy/luci-app-netdata.git package/lean/luci-app-netdata

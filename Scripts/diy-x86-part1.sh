20d878d86ab5d96659203f6e2ce36adb13af1a70
jiawm <42288069+jiawm@users.noreply.github.com>
2021-05-13 17:46:33 +0800

    Update diy-x86-part1.sh

--- Scripts/diy-x86-part1.sh
+++ Scripts/diy-x86-part1.sh
@@ -16,8 +16,8 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/lienol
-
-git clone https://github.com/jiawm/luci-app-poweroff.git package/poweroff
+git clone https://github.com/tty228/luci-app-serverchan.git package/custom/serverchan
+#git clone https://github.com/jiawm/luci-app-poweroff.git package/poweroff
 git clone https://github.com/linkease/nas-packages.git package/nas #添加ddnsto和linkease
 git clone https://github.com/jiawm/sirpdboy-package.git package/sirpdboy
 #替换为sirpdboy中文版netdata
